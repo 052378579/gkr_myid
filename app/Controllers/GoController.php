de7a58f43bb764757661dedd5a780ceef86025ed
budi <budiasli@gmail.com>
2026-08-21 10:46:09 +0700

    Update: 21.08.2026

---
+++ app/Controllers/GoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class GoController extends BaseController
+{
+    public function erp($kode)
+    {
+        return redirect()->to("http://103.39.49.86:82/desk#Form/Item/" . $kode);
+    }
+}
+
