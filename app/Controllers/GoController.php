743c4b892d2fc6db6c7289a3960cd355e86ca24a
budi <budiasli@gmail.com>
2026-09-03 02:42:02 +0700

    Update: 03.09.2026

--- app/Controllers/GoController.php
+++ app/Controllers/GoController.php
@@ -6,7 +6,15 @@ class GoController extends BaseController
 {
     public function erp($kode)
     {
-        return redirect()->to("http://103.39.49.86:82/desk#Form/Item/" . $kode);
+        // Bersihkan kode dari spasi/enter yang mungkin terbawa dari n8n
+        $clean_kode = trim(urldecode($kode));
+
+        // Rute khusus untuk cetak BOM (Case Insensitive & Tahan Spasi)
+        if (strpos(strtoupper($clean_kode), 'BOM-') === 0) {
+            return redirect()->to("http://103.39.49.86:82/printview?doctype=BOM&name=" . urlencode($clean_kode) . "&format=BOM%20Rincian&no_letterhead=0");
+        }
+        
+        // Rute standar untuk katalog Item
+        return redirect()->to("http://103.39.49.86:82/desk#Form/Item/" . urlencode($clean_kode));
     }
 }
-
